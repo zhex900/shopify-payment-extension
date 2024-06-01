ec8c4a1a67a07681060cbaa5ecd1311b4a07422e
Jake He <jake.he@nextgen.group>
2024-06-01 22:22:44 +1000

    github action deploy

--- infrastructure/application-stack/remix.ts
+++ infrastructure/application-stack/remix.ts
@@ -23,21 +23,24 @@ const shopifyApiSecret = new sst.Secret(
   process.env.SHOPIFY_API_SECRET,
 );
 
-export const remix = new sst.aws.Remix("ShopifyApp", {
-  link: [sessionsTable, shopifyApiKey, shopifyApiSecret],
-  permissions: [
-    // read parameter from ssm
-    {
-      actions: ["ssm:GetParameter"],
-      resources: [appUrlParameter.arn],
+export const remix = new sst.aws.Remix(
+  `ShopifyApp${$app.stage.toUpperCase()}`,
+  {
+    link: [sessionsTable, shopifyApiKey, shopifyApiSecret],
+    permissions: [
+      // read parameter from ssm
+      {
+        actions: ["ssm:GetParameter"],
+        resources: [appUrlParameter.arn],
+      },
+    ],
+    environment: {
+      SHOPIFY_APP_URL_PARAMETER_NAME: appUrlParameter.name,
+      SHOPIFY_APP_URL: process.env.SHOPIFY_APP_URL || "",
+      SHOP_CUSTOM_DOMAIN: process.env.SHOP_CUSTOM_DOMAIN || "",
+      SHOPIFY_SCOPES: shopifyAppConfig.access_scopes.scopes || "",
+      SESSIONS_TABLE_NAME: sessionsTable.name,
+      REGION: "ap-southeast-2",
     },
-  ],
-  environment: {
-    SHOPIFY_APP_URL_PARAMETER_NAME: appUrlParameter.name,
-    SHOPIFY_APP_URL: process.env.SHOPIFY_APP_URL || "",
-    SHOP_CUSTOM_DOMAIN: process.env.SHOP_CUSTOM_DOMAIN || "",
-    SHOPIFY_SCOPES: shopifyAppConfig.access_scopes.scopes || "",
-    SESSIONS_TABLE_NAME: sessionsTable.name,
-    REGION: "ap-southeast-2",
   },
-});
+);
--- infrastructure/application-stack/ssm.ts
+++ infrastructure/application-stack/ssm.ts
@@ -2,4 +2,5 @@ export const appUrlParameter = new aws.ssm.Parameter("AppUrlParameter", {
   name: `/${$app.name}/${$app.stage}/app-url`,
   type: "String",
   value: "remix.url",
+  overwrite: true,
 });
